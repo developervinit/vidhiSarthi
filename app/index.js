7f7d8ee5fb9999fa007f75cfaffee3efd24ef98f
developervinit <vinit91.dev@gmail.com>
2024-09-19 20:05:25 +0530

    restore the slug name in app.json and restore the code index.js code from delaying splash screen

--- app/index.js
+++ app/index.js
@@ -1,34 +1,5 @@
-import React, { useEffect, useState } from "react";
-import * as SplashScreen from "expo-splash-screen";
 import Home from "./Home";
 
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
-
 export default function Index() {
-  const [isAppReady, setAppReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Artificially delay for 1 second to simulate loading
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // After the 1-second delay, hide the splash screen
-        setAppReady(true);
-        await SplashScreen.hideAsync();
-      }
-    }
-
-    prepare();
-  }, []);
-
-  if (!isAppReady) {
-    return null; // Keep the app in a loading state until the splash screen is hidden
-  }
-
-  // Once app is ready, render the Home screen
   return <Home />;
 }
