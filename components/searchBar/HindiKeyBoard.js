0cab8f306874a0aed44c9914cdb39b63eb491ac3
developervinit <vinit91.dev@gmail.com>
2024-10-28 00:10:07 +0530

    chore: add hindi charecter to hindi keyboard

--- components/searchBar/HindiKeyBoard.js
+++ components/searchBar/HindiKeyBoard.js
@@ -1,14 +1,65 @@
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
 export default function HindiKeyBoard({ hindiKeyboard, addHindiCharacter }) {
-  const hindiChar = ["क", "ख", "ग", "घ", "ङ", "च", "छ", "ज", "झ", "ञ", "ठ", "ड", "ढ", "ण", "त", "द", "ध", "न", "प", "फ", "ब", "भ", "म", "य", "र", "ल", "व", "श", "ष", "स", "ह", "क्ष", "त्र", "ज्ञ", "श्र", "अ", "इ", "उ", "ऋ", "ए"];
+  const hindiChar = [
+    "क",
+    "ख",
+    "ग",
+    "घ",
+    "ङ",
+    "च",
+    "छ",
+    "ज",
+    "झ",
+    "ञ",
+    "ट",
+    "ठ",
+    "ड",
+    "ढ",
+    "ण",
+    "त",
+    "द",
+    "ध",
+    "न",
+    "प",
+    "फ",
+    "ब",
+    "भ",
+    "म",
+    "य",
+    "र",
+    "ल",
+    "व",
+    "श",
+    "ष",
+    "स",
+    "ह",
+    "क्ष",
+    "त्र",
+    "ज्ञ",
+    "श्र",
+    "अ",
+    "इ",
+    "उ",
+    "ऋ",
+    "ए",
+  ];
 
   return (
     <View>
       {hindiKeyboard ? (
         // Set a fixed height for the scrollable area
         <View style={styles.keyboardContainer}>
-          <ScrollView contentContainerStyle={styles.scrollContainer} keyboardShouldPersistTaps="handled">
+          <ScrollView
+            contentContainerStyle={styles.scrollContainer}
+            keyboardShouldPersistTaps="handled"
+          >
             <View style={styles.hindiKeyboard}>
               {hindiChar.map((char) => {
                 return (
