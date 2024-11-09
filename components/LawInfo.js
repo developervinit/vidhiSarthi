d2e902eaa9f384081221e1f50a738cddbf8f283a
developervinit <vinit91.dev@gmail.com>
2024-11-09 10:18:30 +0530

    refactor: add informational data in LawInfo and enhace the style

--- components/LawInfo.js
+++ components/LawInfo.js
@@ -3,16 +3,28 @@ import highlightText from "../helper/highlightText.js";
 import { howToContentColor } from "../constants/colors.js";
 
 export default LawInfo = ({ content }) => {
-  return content.map((item, index) => {
-    return (
-      <View style={styles.lawInfoContainer} key={index}>
-        <Text style={styles.lawInfoContentWrapper}>
-          <Text style={styles.newLineBulletsStyle}>{index + 1}. </Text>
-          {highlightText(item)}
+  return (
+    <>
+      <View>
+        <Text style={styles.noticeTextWrapper}>
+          <Text style={styles.noticeTextHead}>सूचना: </Text>
+          {highlightText(
+            "%जोडी% गयी धाराऐं एवं उपधाराऐं %हरे रंग% में दर्शायी गई है और $हटायी$ गयी धाराऐं एवं उपधाराऐं $लाल रंग$ में दर्शायी गई है।"
+          )}
         </Text>
       </View>
-    );
-  });
+      {content.map((item, index) => {
+        return (
+          <View style={styles.lawInfoContainer} key={index}>
+            <Text style={styles.lawInfoContentWrapper}>
+              <Text style={styles.newLineBulletsStyle}>{index + 1}. </Text>
+              {highlightText(item)}
+            </Text>
+          </View>
+        );
+      })}
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -21,12 +33,21 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   lawInfoContentWrapper: {
-    fontSize: 18,
+    fontSize: 20,
     textAlign: "justify",
     color: howToContentColor,
   },
   newLineBulletsStyle: {
     fontWeight: "bold",
     fontSize: 18,
+  },
+  noticeTextWrapper: {
+    fontSize: 20,
+    textAlign: "justify",
+    color: howToContentColor,
+    marginBottom: 10,
+  },
+  noticeTextHead: {
+    fontWeight: "bold",
   },
 });
