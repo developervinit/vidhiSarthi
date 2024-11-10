61277ae0b18aed324a6f9dc78f6d2e996dfbebe1
developervinit <vinit91.dev@gmail.com>
2024-11-10 13:22:16 +0530

    refactor: separate the data used to shaow error when user search invalid section

--- components/ErrorDisplay.js
+++ components/ErrorDisplay.js
@@ -1,39 +1,7 @@
 import { View, Text, StyleSheet } from "react-native";
+import lawDataForErrorDisplay from "../data/errorData/wrongSectionErrorData.js";
 
 export default function ErrorDisplay({ inputValue, law, codeOfSectionType }) {
-  const lawInformation = [
-    {
-      oldLaw: {
-        lawN: "इंडियन एवीडेंस एक्ट 1872",
-        range: "1 से 167",
-      },
-      newLaw: {
-        lawN: "भारतीय साक्ष्य अधिनियम 2023",
-        range: "1 से 170",
-      },
-    },
-    {
-      oldLaw: {
-        lawN: "भारतीय दण्ड संहिता, 1860",
-        range: "1 से 511",
-      },
-      newLaw: {
-        lawN: "भारतीय न्याय संहिता, 2023",
-        range: "1 से 356",
-      },
-    },
-    {
-      oldLaw: {
-        lawN: "दण्ड प्रक्रिया संहिता 1873",
-        range: "1 से 484",
-      },
-      newLaw: {
-        lawN: "भारतीय नागरिक सुरक्षा संहिता 2023",
-        range: "1 से 533",
-      },
-    },
-  ];
-
   // Define a mapping for the law names to indexes in lawInformation
   const lawIndexMap = {
     sakshyaAdhiniyam: 0,
@@ -48,8 +16,8 @@ export default function ErrorDisplay({ inputValue, law, codeOfSectionType }) {
   const lawType = codeOfSectionType === "prevCode" ? "oldLaw" : "newLaw";
 
   // Access the law information dynamically
-  lawName = lawInformation[lawIndex][lawType]["lawN"];
-  sectionRange = lawInformation[lawIndex][lawType]["range"];
+  lawName = lawDataForErrorDisplay[lawIndex][lawType]["lawN"];
+  sectionRange = lawDataForErrorDisplay[lawIndex][lawType]["range"];
 
   return (
     <View style={styles.errorCard}>
